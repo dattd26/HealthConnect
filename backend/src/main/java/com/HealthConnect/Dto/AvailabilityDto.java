2074bb1610a44f4b895ada957c551f3f8780968f
-v <ducdat05112004@gmail.com>
2025-06-16 21:07:11 +0700

    thời gian rãnh trong tuần của bac si

---
+++ backend/src/main/java/com/HealthConnect/Dto/AvailabilityDto.java
@@ -0,0 +1,13 @@
+package com.HealthConnect.Dto;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+import lombok.Data;
+
+@Data
+public class AvailabilityDto {
+    private DayOfWeek dayOfWeek;
+    private LocalTime startTime;
+    private LocalTime endTime;
+}
--- backend/src/main/java/com/HealthConnect/Model/Doctor.java
+++ backend/src/main/java/com/HealthConnect/Model/Doctor.java
@@ -1,16 +1,15 @@
 package com.HealthConnect.Model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+
 
 @Getter
 @Setter
@@ -20,15 +19,20 @@ public class Doctor extends User {
     @ManyToMany
     @JoinTable(
         name = "doctors_specialties",
-        joinColumns = @jakarta.persistence.JoinColumn(name = "doctor_id"),
-        inverseJoinColumns = @jakarta.persistence.JoinColumn(name = "specialty_id")
+        joinColumns = @JoinColumn(name = "doctor_id"),
+        inverseJoinColumns = @JoinColumn(name = "specialty_id")
     )
-    @JsonBackReference(value = "doctor-spectialties")
-    private List<MedicalSpecialty> specialties;
+    @JsonBackReference(value = "doctor-specialties") 
+    private Set<MedicalSpecialty> specialties = new HashSet<>(); 
 
     private String license;
     private String hospital;
+
     @OneToMany(mappedBy = "doctor")
-    @JsonBackReference(value = "doctor-timeslots")
-    private List<DoctorTimeslot> timeslots;
+    @JsonBackReference(value = "doctor-availabilities")
+    private List<DoctorAvailability> availabilities;
+
+    // @OneToMany(mappedBy = "doctor")
+    // @JsonBackReference(value = "doctor-slots")
+    // private Set<AppointmentSlot> slots = new HashSet<>();
 }
