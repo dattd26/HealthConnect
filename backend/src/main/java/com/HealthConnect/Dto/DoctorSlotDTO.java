e848b1713195f597bc1fc5b107ece1cfde287d0d
-v <ducdat05112004@gmail.com>
2025-06-17 20:42:09 +0700

    chore: format time

--- backend/src/main/java/com/HealthConnect/Dto/DoctorSlotDTO.java
+++ backend/src/main/java/com/HealthConnect/Dto/DoctorSlotDTO.java
@@ -4,6 +4,8 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
@@ -11,7 +13,9 @@ public class DoctorSlotDTO {
     // private Long id;
     private Long doctorId;
     private LocalDate date;
+    @JsonFormat(pattern = "HH:mm")
     private LocalTime startTime;
+    @JsonFormat(pattern = "HH:mm")
     private LocalTime endTime;
     private Duration duration;
     private String status;
